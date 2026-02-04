78220f06393c0fa1220bed8f681d2dd103df2d00
dowelani <dowelanikhumbelo@gmail.com>
2026-02-04 13:08:07 +0200

    valid file

--- frontend/src/components/TransactionUpload.js
+++ frontend/src/components/TransactionUpload.js
@@ -13,7 +13,7 @@ export default function TransactionUpload({ onDataReady }) {
 
     // Validate file type
     if (!/\.(csv|xlsx?|xls)$/i.test(file.name)) {
-      setError("Invalid file type. Please upload a validfile.");
+      setError("Invalid file type. Please upload a valid file.");
       onDataReady([]); // clear previous
       return;
     }
